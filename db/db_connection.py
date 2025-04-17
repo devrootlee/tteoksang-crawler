09d7acda81f6c8b9db3d57dab3d1d72224672113
leeroot <dev.rootlee@gmail.com>
2025-04-17 13:23:37 +0900

    fix: db_connection 수정

---
+++ db/db_connection.py
@@ -0,0 +1,12 @@
+import os
+
+import psycopg2
+
+def db_local_connection():
+    return psycopg2.connect(
+        host=os.environ.get("DB_HOST"),
+        port=5432,
+        dbname=os.environ.get("DB"),
+        user=os.environ.get("DB_USERNAME"),
+        password=os.environ.get("DB_PASSWORD")
+    )
